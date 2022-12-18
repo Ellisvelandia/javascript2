ee11ddd625421f943ecd3368ded27ad1defed5d3
Ellisvelandia <eyis619l@gmail.com>
2022-12-17 23:19:24 -0500

    javascript

---
+++ code.js
@@ -0,0 +1,46 @@
+'use strict'
+
+console.log("Hello world");
+
+let box = "iam box"
+
+box = "boxess"
+
+console.log(box)
+
+/*
+String - word or letter 
+Number
+Boolean - true or false 
+undefined
+null
+*/
+
+let name = "ellis";
+let number = 29;
+let trues = true;
+let falses = false;
+let undefineds = undefined;
+let nulls = null;
+
+console.log(`${name} ${number} + true: ${trues}, false: ${falses}, undefined: ${undefineds}, null: ${nulls}`);
+
+let number1 = 5
+let number2 = 50
+let sum = number1 * number2
+console.log(sum)
+
+let person = {
+  name: "ellis",
+  lastName: "caicedo"
+}
+
+// Array
+
+console.log(person["name"])
+console.log(person.lastName)
+
+let numbers= 5 
+let numberss = [11,2,3,4,5]
+console.log(numberss[0] + numberss[1])
+
